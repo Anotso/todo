bf9834e6ee70c7db88440e550bd7bf908b024ae9
Anotso <40184359+Anotso@users.noreply.github.com>
2020-08-11 17:12:04 -0300

    ToDo concluido. Funcionalidades básicas prontas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import List from './components/List';
 import TodoForm from './components/TodoForm';
 import Item from './components/Item';
+import Modal from './components/Modal';
+
+const SAVED_ITEMS = "savedItems";
 
 function App() {
 
   const [items, setItems] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
-  function onAddItem(text){
+  useEffect(() => {
+    let savedItems = JSON.parse(localStorage.getItem(SAVED_ITEMS));
+    if (savedItems) {
+      setItems(savedItems);
+    }
+  }, []);
+
+  //DESSA FORMA SE PEGA OS DADOS DO LOCALSTORAGE, E QUANDO SE PASSA O ARRAY VAZIO ELE EXECUTA SOMENTE NA INICIALIZAÇÃO
+  //getItem => É UM METODO DO LOCALSTORAGE. NÃO POSSUI RELAÇÃO COM O CÓDIGO JÁ ESCRITO
+
+  useEffect(() => {
+    localStorage.setItem(SAVED_ITEMS, JSON.stringify(items));
+  }, [items]);
+
+  function onAddItem(text) {
     let item = new Item(text);
     setItems([...items, item]);
+    onHideModal();
   }
 
-  function onItemDeleted(item){
-    let filteredItems = items.filter(it=>it.id !== item.id);
+  function onItemDeleted(item) {
+    let filteredItems = items.filter(it => it.id !== item.id);
     setItems(filteredItems);
   }
 
-  function onDone(item){
-    let updateItems = items.map(it=>{
-      if(it.id === item.id){
+  function onDone(item) {
+    let updateItems = items.map(it => {
+      if (it.id === item.id) {
         it.done = !it.done;
       }
       return it;
@@ -28,11 +47,20 @@ function App() {
     setItems(updateItems);
   }
 
+  function onHideModal(e) {
+    setShowModal(false);
+  }
+
   return (
     <div className="container">
-      <h1>Todo</h1>
-      <TodoForm onAddItem={onAddItem}></TodoForm>
+      <header className="header">
+        <h1>Todo</h1>
+        <button className="addButton" onClick={()=>{setShowModal(true)}}>+</button>
+      </header>
       <List onDone={onDone} onItemDeleted={onItemDeleted} items={items}></List>
+      <Modal show={showModal} onHideModal={onHideModal}>
+        <TodoForm onAddItem={onAddItem}></TodoForm>
+      </Modal>
     </div>
   );
 }
